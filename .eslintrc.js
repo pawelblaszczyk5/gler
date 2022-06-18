51fa97308f9b8bf73be76aec3e32184e05d05a1b
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-06-18 14:27:29 +0200

    chore: remove unnecessary packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,51 @@
 module.exports = {
   root: true,
-  extends: '@stomya/eslint-config',
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:jsx-a11y/strict',
+    'plugin:solid/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'react', 'solid'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'react/self-closing-comp': [
+      'error',
+      {
+        component: true,
+        html: true,
+      },
+    ],
+    'newline-after-var': 2,
+    '@typescript-eslint/consistent-type-imports': 2,
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'TSEnumDeclaration',
+        message: "Don't declare enums",
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': ['error', { vars: 'all', args: 'after-used', ignoreRestSiblings: true }],
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,11 @@
-const config = require('@stomya/prettier-config');
-
-module.exports = config;
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  singleAttributePerLine: true,
+};
